1c8f4f9062f9e1eee084f880ea64832b10ba2d18
shaina2005 <0506shyna@gmail.com>
2026-08-17 14:25:04 +0530

    prepare backend for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'API is running' })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
